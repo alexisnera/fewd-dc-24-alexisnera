d38d834ae8d8646c9980444d44f44d0f41380b4e
Alexis Nera <alexisdnera@gmail.com>
2016-07-07 14:16:37 -0400

    In-class exercises sync on laptop

--- Week_06_Review_Refactor/11_ advance_arrays/starter/animals/js/index.js
+++ Week_06_Review_Refactor/11_ advance_arrays/starter/animals/js/index.js
@@ -1,19 +1,10 @@
-jQuery(document).ready(function(){
 
 	// Create variables - images, maximum amount of images
 	var images = (['images/animal1.jpg', 'images/animal2.jpg', 'images/animal3.jpg', 'images/animal4.jpg', 'images/animal5.jpg', 'images/animal6.jpg', 'images/animal7.jpg']);
 	var i = 0;
 	var maxImages = images.length-1;
 
-
-	// Bind the function to an on-click event
-	jQuery('#buttons-wrapper #nextButton').click(nextImage);
-	jQuery('#buttons-wrapper #backButton').click(prevImage);
-
-});
-
-
-// Create function for next image
+	// Create function for next image
 	function nextImage(){
 		if (i < maxImages ) { 
 			i++;
@@ -39,3 +30,12 @@ jQuery(document).ready(function(){
 		function changeImage(){
 			jQuery("#image-to-vote-on").attr('src',images[i]);
 		}
+
+
+jQuery(document).ready(function(){
+
+	// Bind the function to an on-click event
+	jQuery('#buttons-wrapper #nextButton').click(nextImage);
+	jQuery('#buttons-wrapper #backButton').click(prevImage);
+
+});
---
+++ Week_06_Review_Refactor/12_lab/starter_code/js/exercise2.js
@@ -0,0 +1,8 @@
+// Create variable 
+
+
+
+// Run function that will pull selected pet data and display on page
+jQuery(document).ready(function(){
+
+})
